46e1cba60c476b303d3725b49e9a56da2b135ceb
Celestin Gabor <127986056+celegabor@users.noreply.github.com>
2023-08-02 18:46:48 +0200

    modifica card info pt2

--- dettagli.js
+++ dettagli.js
@@ -7,8 +7,17 @@ function mostraDettagliLibro() {
     const title = params.get("title");
 
     // Crea gli elementi HTML per visualizzare i dettagli del libro
+    const dettagliLibroDiv = document.getElementById("dettagliLibro");
+
+    const divCard = document.createElement("div");
+    divCard.classList.add("cardDettails")
+
     const imageElement = document.createElement("img");
     imageElement.src = imageUrl;
+
+    const imageUrlLink = document.createElement("p");
+    imageUrlLink.innerText = "URL immagine: " + imageUrl;
+
 
     const priceElement = document.createElement("p");
     priceElement.innerText = "Prezzo: $" + price;
@@ -16,11 +25,12 @@ function mostraDettagliLibro() {
     const titleElement = document.createElement("h2");
     titleElement.innerText = title;
 
-    // Aggiungi gli elementi HTML al div dei dettagli del libro
-    const dettagliLibroDiv = document.getElementById("dettagliLibro");
-    dettagliLibroDiv.appendChild(imageElement);
-    dettagliLibroDiv.appendChild(priceElement);
-    dettagliLibroDiv.appendChild(titleElement);
+    divCard.appendChild(imageElement);
+    divCard.appendChild(imageUrlLink);
+    divCard.appendChild(priceElement);
+    divCard.appendChild(titleElement);
+    
+    dettagliLibroDiv.appendChild(divCard)
 }
 
 
